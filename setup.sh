cdb6f5dcb85f73dc4b4103786f8b11bd79da1067
pkirby6 <pkirby6@github.gatech.edu>
2026-04-23 20:21:28 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -e
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
+bash /tmp/miniconda.sh -b -p /root/miniconda3
+
+source /root/miniconda3/etc/profile.d/conda.sh
+
+echo "alias miniconda='source /root/miniconda3/etc/profile.d/conda.sh && conda activate base'" >> /root/.bashrc
 
 conda env create -f environment.yml || conda env update -f environment.yml
+
 conda run -n contrastive-av python -m pip install \
   "https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.9.0/flash_attn-2.8.3+cu128torch2.10-cp312-cp312-linux_x86_64.whl"
-
