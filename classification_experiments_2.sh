1154a510a3301bf1fb650962c35a25249314f822
pkirby6 <pkirby6@github.gatech.edu>
2026-04-25 03:42:03 -0700

    fix lr arg

--- classification_experiments_2.sh
+++ classification_experiments_2.sh
@@ -16,7 +16,7 @@ for lr in "${learning_rates[@]}"; do
       --trainer.accelerator=cuda \
       --trainer.devices=1 \
       --data.batch_size="$bs" \
-      --optimizer.init_args.lr="$lr" \
+      --model.lr="$lr" \
       --trainer.logger.init_args.name="$run_name"
   done
 done
