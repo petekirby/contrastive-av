7b926626316459006e1cfff381c935e2d73bbd48
pkirby6 <pkirby6@github.gatech.edu>
2026-04-25 01:08:36 -0700

    test classification focal

--- classification_experiments_1.sh
+++ classification_experiments_1.sh
@@ -8,11 +8,11 @@ negatives_per_positive=(1 2 4)
 
 for bs in "${batch_sizes[@]}"; do
   for npp in "${negatives_per_positive[@]}"; do
-    run_name=$(printf "classification-bs=%d-npp=%d" "$bs" "$npp")
+    run_name=$(printf "classification-bs=%d-npp=%d-focal" "$bs" "$npp")
     echo "Running ${run_name} on CUDA device ${cuda_device}"
 
     CUDA_VISIBLE_DEVICES="$cuda_device" python train.py fit \
-      --config="configs/tinybert_classification.yaml" \
+      --config="configs/tinybert_classification_focal.yaml" \
       --trainer.accelerator=cuda \
       --trainer.devices=1 \
       --data.batch_size="$bs" \
