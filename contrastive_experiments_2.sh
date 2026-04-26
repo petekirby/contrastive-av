4902ebcd18749abe2fc00d1e1a459dafe229c301
pkirby6 <pkirby6@github.gatech.edu>
2026-04-25 19:09:10 -0700

    configurable LR for experiment

--- contrastive_experiments_2.sh
+++ contrastive_experiments_2.sh
@@ -15,43 +15,45 @@ run_experiment() {
     "$@"
 }
 
-#run_experiment "contrastive-bs=512-lr=8e-5" \
-#  --trainer.max_epochs=10 \
-#  --model.init_args.microbatch_size=256 \
-#  --data.batch_size=512 \
-#  --model.init_args.lr=8e-5
+lr="$1"
 
-#run_experiment "contrastive-bs=1024-lr=8e-5" \
-#  --trainer.max_epochs=10 \
-#  --model.init_args.microbatch_size=256 \
-#  --data.batch_size=1024 \
-#  --model.init_args.lr=8e-5
+run_experiment "contrastive-bs=512-lr=$lr" \
+  --trainer.max_epochs=10 \
+  --model.init_args.microbatch_size=256 \
+  --data.batch_size=512 \
+  --model.init_args.lr="$lr"
+
+run_experiment "contrastive-bs=1024-lr=$lr" \
+  --trainer.max_epochs=10 \
+  --model.init_args.microbatch_size=256 \
+  --data.batch_size=1024 \
+  --model.init_args.lr="$lr"
 
 # Long Baseline: batch size 256
-run_experiment "long-contrastive-bs=256-lr=8e-5" \
+run_experiment "long-contrastive-bs=256-lr=$lr" \
   --trainer.max_epochs=40 \
   --data.batch_size=256 \
-  --model.init_args.lr=8e-5
+  --model.init_args.lr="$lr"
 
 # GradCache
-run_experiment "long-contrastive-bs=1024-lr=8e-5" \
+run_experiment "long-contrastive-bs=1024-lr=$lr" \
   --trainer.max_epochs=40 \
   --model.init_args.microbatch_size=256 \
   --data.batch_size=1024 \
-  --model.init_args.lr=8e-5
+  --model.init_args.lr="$lr"
 
 # Gradient accumulation
-run_experiment "long-contrastive-accum=1024-lr=8e-5" \
+run_experiment "long-contrastive-accum=1024-lr=$lr" \
   --trainer.max_epochs=40 \
   --data.batch_size=256 \
   --trainer.accumulate_grad_batches=4 \
-  --model.init_args.lr=8e-5
+  --model.init_args.lr="$lr"
 
 # Cross-Batch Memory
-run_experiment "long-contrastive-xbm=1024-lr=8e-5" \
+run_experiment "long-contrastive-xbm=1024-lr=$lr" \
   --trainer.max_epochs=40 \
   --model.init_args.cross_batch_memory_size=1024 \
   --data.batch_size=256 \
-  --model.init_args.lr=8e-5
+  --model.init_args.lr="$lr"
 
 echo "Done."
